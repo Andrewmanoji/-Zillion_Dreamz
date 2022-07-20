60b9774a172e6dfb05fbfc0d29f3ffe652e9fb71
MANOJ-S-2002 <manoj.23it@licet.ac.in>
2022-07-21 00:56:54 +0530

    updated

--- src/components/dreamCommunity/BrainstormingFamily.jsx
+++ src/components/dreamCommunity/BrainstormingFamily.jsx
@@ -28,67 +28,91 @@ export default function BrainstormingFamily({ setBrainstroming, mob }) {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <ArrowBackIcon
-        className="bg-purple rounded-circle p-1 me-3 mt-2 cursor-pointer li-shadow"
-        sx={{ fontSize: 30 }}
-        onClick={() => setBrainstroming(false)}
-      />
-
-      {/* Full container */}
-      <div className="container my-4">
-        {/* upper half */}
-        <div className="d-flex  justify-content-between   py-3">
-          {/* Zd menu  */}
-          <div className="  d-flex justify-content-between col-6 ">
-            <span className="fw-bold purple-gradient fs-3">
-              Select Your ZD Menu
-            </span>
-
-            <FormControl variant="filled" sx={{ minWidth: 150 }}>
-              <InputLabel id="demo-simple-select-filled-label">
-                ZD Menu
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-filled-label"
-                id="demo-simple-select-filled"
-                className="mb-4"
-                value={menu}
-                label="menu"
-                onChange={handleChange}
-              >
-                <MenuItem value={10}>General</MenuItem>
-                <MenuItem value={20}>Business</MenuItem>
-                <MenuItem value={30}>Education</MenuItem>
-              </Select>
-            </FormControl>
-          </div>
-
-          {/* button */}
-          <div className="bg-light rounded-3  mx-3 p-3 col-4  py-3 d-flex flex-column  ">
-            <Button
-              className="btn me-4 mb-4 justify-center"
-              variant="outlined"
-              onClick={() => {
-                setIdea(false);
-              }}
-            >
-              Query Maker
-            </Button>
-            <Button
-              className="btn mb-4"
-              variant="outlined"
-              onClick={() => {
-                setIdea(true);
-              }}
-            >
-              Your queries
-            </Button>
-          </div>
-        </div>
-
-        {idea ? <YourQueries /> : <QueryMaker />}
+    <>
+      {/* header */}
+      <div
+        className=" mb-2 mt-1 mt-md-2 col-11 mx-auto d-flex pe-4"
+        // style={{ borderTop: "2px solid #7201c8" }}
+      >
+        <ArrowBackIcon
+          className="bg-purple rounded-circle p-1 me-3 mt-2 cursor-pointer li-shadow"
+          sx={{ fontSize: 30 }}
+          onClick={() => setBrainstroming(false)}
+        />
+        <p
+          className="fw-bold bg-linearlr col-6 col-md-3 col-xl-2 mx-auto text-center p-2 mt-0"
+          style={{
+            borderRadius: "0px 0px 10px 10px",
+            boxShadow: "0px 5px 10px #0000005e",
+          }}
+        >
+          ZD Brainwave
+        </p>
       </div>
-    </ThemeProvider>
+
+      {/* main */}
+
+      <ThemeProvider theme={theme}>
+        {/* Full container */}
+        <div className="container my-4">
+          {/* upper half */}
+          <div className="d-flex  justify-content-between   py-3">
+            {/* Zd menu  */}
+            <div className="  d-flex justify-content-between col-6 ">
+              <span className="fw-bold purple-gradient fs-3">
+                Select Your ZD Menu
+              </span>
+
+              <FormControl variant="filled" sx={{ minWidth: 150 }}>
+                <Select
+                  labelId="demo-simple-select-filled-label"
+                  id="demo-simple-select-filled"
+                  className="mb-4"
+                  value={menu}
+                  label="menu"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={10}>General</MenuItem>
+                  <MenuItem value={20}>Business</MenuItem>
+                  <MenuItem value={30}>Education</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+
+            {/* button */}
+            <div 
+            className="bg-grey rounded-3  mx-3 px-4 py-2 col-4  py-3 d-flex flex-column  ">
+              <Button
+               style={{
+            borderRadius: "0px 0px 10px 10px",
+            boxShadow: "0px 5px 10px #0000005e"}}
+             className="fw-bold bg-linearlr col-6 col-md-3 col-xl-2 mx-auto text-center p-5 mt-0 bg-grey rounded-3  mx-3 px-4 py-2 col-4  py-3 d-flex flex-column "
+                variant="outlined"
+                onClick={() => {
+                  setIdea(false);
+                }}
+              >
+                Query Maker
+              </Button>
+
+              <Button
+                style={{
+            borderRadius: "0px 0px 10px 10px",
+            boxShadow: "0px 5px 10px #0000005e"}}
+             className="fw-bold bg-linearlr col-6 col-md-3 col-xl-2 mx-auto text-center p-2 mt-0 bg-grey rounded-3  mx-3 px-4 py-2 col-4  py-3 d-flex flex-column "
+                variant="outlined"
+                onClick={() => {
+                  setIdea(true);
+                }}
+              >
+                Idea Crate
+              </Button>
+            </div>
+          </div>
+
+          {idea ? <YourQueries /> : <QueryMaker />}
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
