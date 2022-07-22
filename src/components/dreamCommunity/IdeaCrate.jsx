786455fc9cd81715fb20ee7d215f51f8cc3b97a3
MANOJ-S-2002 <manoj.23it@licet.ac.in>
2022-07-22 18:06:56 +0530

    updated

--- src/components/dreamCommunity/IdeaCrate.jsx
+++ src/components/dreamCommunity/IdeaCrate.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import LightbulbIcon from "@mui/icons-material/Lightbulb";
 import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
@@ -13,6 +14,8 @@ import * as React from "react";
 import DialogActions from "@mui/material/DialogActions";
 import { endpoint, token, config } from "../../endpoint";
 import axios from "axios";
+import Avatar from "@mui/material/Avatar";
+
 
 const PurpleTooltip = styled(({ className, ...props }) => (
   <Tooltip {...props} classes={{ popper: className }} />
@@ -29,6 +32,7 @@ export default function IdeaCrate({ setOpen }) {
   const dispatch = useDispatch();
   const history = useHistory();
   const [events, setEvents] = useState(false);
+  const data = useSelector((state) => state.queryMaker);
 
   const [userData, setUserData] = useState({
     query: "",
@@ -58,11 +62,107 @@ export default function IdeaCrate({ setOpen }) {
   return (
     <>
       <div className="rounded-4 bg-purple vh-40 position-relative pt-1">
-       
-       
         <div>
-          
-          
+          <>
+            <div className="rounded-4 bg-purple d-flex flex-column justify-content-evenly p-4 vh-70 w-100">
+              {data &&
+                data.map((data, index) => (
+                  <div className="d-flex  mb-2   align-items-start" key={index}>
+                    {/* user name */}
+                    <div className=" p-2 ps-1  pb-1 mb-sm-0 pb-sm-0  col-1     ">
+                      <Avatar
+                        className="bg-linear  "
+                        // alt={logUser.username}
+                        // src={logUser.profile_pic.public_url}
+                        style={{
+                          boxShadow: "0px 5px 10px black",
+                          transform: "scale(1.2)",
+                          width: 50,
+                          height: 50,
+                        }}
+                      />
+                    </div>
+
+                    <div className="d-flex flex-column  col-11 p-0 pl-0">
+                      {/* </StyledBadge> */}
+                      <div className="d-flex flex-sm-row mt-0  ms-0 pe-1 pe-sm-2 mb-sm-3   ">
+                        <div
+                          className="fw-bold pe-sm-2  d-flex "
+                          style={{ fontSize: 18 }}
+                        >
+                          <span
+                            className="fw-bold pe-sm-2 p-1 "
+                            style={{ fontSize: 18 }}
+                          >
+                            username
+                          </span>
+                          {/* {logUser.username} */}
+                        </div>
+                      </div>
+                      {/* queries */}
+                      <div className="bg-white  p-4 rounded-3 w-100">
+                        <p className="text-dark mb-0">{data.query}</p>
+                      </div>
+                      
+                    </div>
+                    {/* 
+              <IconButton
+                aria-label="add"
+                size="large"
+                type="button"
+                onClick={() => {
+                  setPublicQueryId(data.id);
+                  setaddOpen(true);
+                }}
+              >
+                <LightbulbIcon fontSize="inherit" className="bg-purple" />
+              
+              </IconButton> */}
+                  </div>
+                ))}
+
+              {/* <Dialog
+          open={addopen}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          fullWidth={true}
+        >
+          <DialogContent>
+            <Ideas data={data} publicQueryId={publicQueryId} />
+          </DialogContent>
+        </Dialog> */}
+              {/* 
+        <div className="d-flex justify-content-end">
+          <PurpleTooltip title="Add Query" className="purple" placement="left">
+            <IconButton
+              aria-label="add"
+              size="large"
+              type="button"
+              onClick={() => setOpen(true)}
+            >
+              <AddIcon fontSize="inherit" className="text-white" />
+            </IconButton>
+          </PurpleTooltip>
+        </div> */}
+            </div>
+
+            {/* <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        fullWidth={true}
+      >
+         <DialogContent>
+         <IdeaCreate setOpen={setOpen} /> 
+        </DialogContent>
+      
+      </Dialog> */}
+          </>
+
+          <div className="divide"></div>
+
           <div className="d-flex justify-content-between m-4 align-items-center">
             <div className="form-floating m-3 w-100 ">
               <input
@@ -74,17 +174,12 @@ export default function IdeaCrate({ setOpen }) {
                 }
               />
               <label className="text-dark" for="floatingInput">
-              Idea Create
+                Idea Create
               </label>
             </div>
           </div>
-          
-          
-          
-          <div className="divide"></div>
+
           <div className="align-items-right justify-content-end mx-5 mt-md-3">
-            
-            
             <DialogActions>
               <Button
                 className="bg-white purple mx-1 mb-3 my-2"
